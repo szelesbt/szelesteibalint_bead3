4cc9021718887eb833a3c033ce9df1eb779678dc
szelesbt <39188883+szelesbt@users.noreply.github.com>
2018-05-11 15:52:32 +0200

    első lépés
    A szinte üres main feltöltése.
    //Vajon működik ez a cucc?

---
+++ main.cpp
@@ -0,0 +1,43 @@
+
+#include <sstream>
+
+#include "graphics.hpp"
+#include "widgetek.h"
+#include "szambeallito.h"
+#include "menu.h"
+#include "szovegszerk.h"
+#include "jelszo.h"
+#include "ablak.h"
+#include "nyomogomb.h"
+#include "simaszoveg.h"
+
+using namespace std;
+using namespace genv;
+
+const int XX(1000),YY(500);
+
+class MyWindow : public Window{
+public:
+
+
+
+
+    MyWindow();
+
+
+};
+
+int main()
+{
+
+    gout.open(XX,YY);
+    MyWindow *mywindow = new MyWindow;
+    mywindow->event_loop();
+    return 0;
+}
+
+MyWindow::MyWindow(){
+
+
+
+  }
